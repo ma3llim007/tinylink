668d7081760836cfddb005953f9a0f303fca0649
ma3llim007 <mohdsameer68257@gmail.com>
2025-11-20 11:04:06 +0530

    chore: added button at stats page

--- frontend/src/pages/Stats.jsx
+++ frontend/src/pages/Stats.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Loading from "../components/Loading";
 import Header from "../components/Header";
 
@@ -25,7 +25,6 @@ const Stats = () => {
             <Header />
             <div className="max-w-2xl mx-auto p-4 bg-slate-900 rounded shadow mt-6">
                 <h1 className="text-2xl font-bold mb-4">Stats for {code}</h1>
-
                 <p>
                     <strong>Target URL:</strong> {data.targetUrl}
                 </p>
@@ -35,6 +34,9 @@ const Stats = () => {
                 <p>
                     <strong>Last Clicked:</strong> {data.lastClickedAt ? new Date(data.lastClickedAt).toLocaleString() : "—"}
                 </p>
+                <Link to={"/"}>
+                    <button class="px-3 py-2 bg-gray-300 text-black mt-4 text-sm hover:bg-gray-100 rounded transition cursor-pointer">Back to Dashboard</button>
+                </Link>
             </div>
         </div>
     );
